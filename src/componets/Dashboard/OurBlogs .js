16754776767b6b3750feb5fc26d9598320df4828
Rahulhawlader <hawladerrahul8@gmail.com>
2022-10-22 17:50:21 +0600

    title change

--- src/componets/Dashboard/OurBlogs .js
+++ src/componets/Dashboard/OurBlogs .js
@@ -2,22 +2,22 @@ import React from 'react';
 import Marquee from "react-easy-marquee";
 
 
-const OurBlogs  = () => {
- 
+const OurBlogs = () => {
+
  return (
   <div className='text-black font-bold'>
-      <Marquee duration={20000}  text='white' height="90px">
-        <div><p>for Admin panel :- </p> 
-        <p>Email:- hawladerrahul8@gmail.com </p>
-        <p>
-  password:-123456 </p>  <p>Please login</p>
+   <Marquee duration={20000} text='white' height="90px">
+    <div><p>for Admin panel :- </p>
+     <p>Email:- hawladerrahul8@gmail.com </p>
+     <p>
+      password:-123456 </p>  <p>Please login</p>
 
-        </div>
-
-        
-      </Marquee>
     </div>
+
+
+   </Marquee>
+  </div>
  );
 };
 
-export default OurBlogs ;
+export default OurBlogs;
--- src/componets/Home/TitleSection/TitleCart.js
+++ src/componets/Home/TitleSection/TitleCart.js
@@ -7,16 +7,16 @@ const TitleCart = () => {
             <div className='grid lg:grid-cols-3  md:grid-cols-2 sm:grid-cols-1  w-11/12 mx-auto '>
 
 
-                <div className='card h-80 w-auto  girls-background  grid place-items-center    shadow-xl transform duration-75 hover:scale-95'>
+                <div className='card h-72 mt-5 w-auto  girls-background  grid place-items-center    shadow-xl transform duration-75 hover:scale-95'>
                     <button className='btn btn-primary '>women</button>
                 </div>
-                <div className='card boys-background  h-80 w-auto img-hover grid place-items-center  shadow-xl
+                <div className='card boys-background  mt-5 h-72 w-auto img-hover grid place-items-center  shadow-xl
             transform duration-75 hover:scale-95
             '>
 
                     <button className='btn btn-primary '>Man</button>
                 </div>
-                <div className='card avarage-background  grid place-items-center h-80 w-auto   shadow-xl
+                <div className='card avarage-background  grid place-items-center h-72 w-auto mt-5   shadow-xl
             
             '>
 
